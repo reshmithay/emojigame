07b8d25cb556cbdd1a58dd1abdf67a27f52a3d31
reshmithay <100594995+reshmithay@users.noreply.github.com>
2022-07-04 01:33:31 +0530

    Update index.js

--- src/components/EmojiGame/index.js
+++ src/components/EmojiGame/index.js
@@ -1,17 +1,3 @@
-/* 
-Quick Tip 
-
-- Use the below function in the EmojiGame Component to shuffle the emojisList every time when an emoji is clicked.
-
-const shuffledEmojisList = () => {
-  const {emojisList} = this.props
-  return emojisList.sort(() => Math.random() - 0.5)
-}
-
-*/
-
-// Write your code here.
-
 import {Component} from 'react'
 import NavBar from '../NavBar'
 import EmojiCard from '../EmojiCard'
